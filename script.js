29e630c7133ade06e42dc15a1eff0f5f9c53b92d
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-03-07 04:07:49 -0500

    menu corrections
    - corrected some visual errors on the hamburguer
    - added more functionalities with javascript

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
 const openMenu = document.querySelector('#menu');
 const closeMenu = document.querySelector('#menuX');
 const menuCont = document.querySelector('#menuCont');
-const liMenu = document.querySelectorAll('#liMenu');
+const ulMenu = document.querySelector('#ulMenu');
+const liMenu = document.querySelectorAll('.liMenu');
+const logo = document.querySelector('#logo');
 /* const menuX = document.querySelector() */
 
 openMenu.addEventListener('click', () => {
   menuCont.classList.add('responsiveMenu');
   openMenu.classList.add('displayNone');
-  closeMenu.classList.add('display');
+  closeMenu.classList.add('display', 'xCenter');
+  ulMenu.classList.add('ulCenter');
 });
 
 closeMenu.addEventListener('click', () => {
   menuCont.classList.remove('responsiveMenu');
   openMenu.classList.remove('displayNone');
-  closeMenu.classList.remove('display');
+  closeMenu.classList.remove('display', 'xCenter');
+});
+
+logo.addEventListener('click', () => {
+  menuCont.classList.remove('responsiveMenu');
+  openMenu.classList.remove('displayNone');
+  closeMenu.classList.remove('display', 'xCenter');
 });
 
 liMenu.forEach((each) => {
   each.addEventListener('click', () => {
     menuCont.classList.remove('responsiveMenu');
     openMenu.classList.remove('displayNone');
-    closeMenu.classList.remove('display');
+    closeMenu.classList.remove('display', 'xCenter');
   });
+});
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth < 768) {
+    menuCont.classList.remove('responsiveMenu');
+    openMenu.classList.remove('displayNone');
+    closeMenu.classList.remove('display', 'xCenter');
+    ulMenu.classList.remove('ulCenter');
+  }
 });
